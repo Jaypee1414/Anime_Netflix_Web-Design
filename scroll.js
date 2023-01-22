4bbf232c7e755106c73049abb24d7fe5d10f552d
Jaypee1414 <jhnplbncyrn03@gmail.com>
2023-01-22 14:18:24 +0800

    first commit

---
+++ scroll.js
@@ -0,0 +1,9 @@
+function scroll_left(){
+    let left = document.querySelector('#first_slide')
+    left.scrollBy(350, 0)
+}
+
+function scroll_right(){
+    let right = document.querySelector('#first_slide')
+    right.scrollBy(-350, 0)
+}
