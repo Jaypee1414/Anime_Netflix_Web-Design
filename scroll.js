ed945af7c2791a78a00293b4158a2427de47b4c9
Jaypee1414 <jhnplbncyrn03@gmail.com>
2023-01-24 14:47:49 +0800

    add popular anime

--- scroll.js
+++ scroll.js
@@ -1,9 +1,9 @@
 function scroll_left(){
-    let left = document.querySelector('#first_slide')
+    let left = document.querySelector('#anime-poster')
     left.scrollBy(350, 0)
 }
 
 function scroll_right(){
-    let right = document.querySelector('#first_slide')
+    let right = document.querySelector('#anime-poster')
     right.scrollBy(-350, 0)
 }
