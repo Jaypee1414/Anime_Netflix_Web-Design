90c1f62a1d81f37799c0dbeb3876e23dd0cf1030
Jaypee1414 <jhnplbncyrn03@gmail.com>
2023-01-25 11:04:21 +0800

    done add international tv shows

--- scroll.js
+++ scroll.js
@@ -1,9 +1,30 @@
 function scroll_left(){
+    let right = document.querySelector('#anime-poster')
+    right.scrollBy(-350, 0)
+}
+
+function scroll_right(){
     let left = document.querySelector('#anime-poster')
     left.scrollBy(350, 0)
 }
 
-function scroll_right(){
-    let right = document.querySelector('#anime-poster')
+
+function IMscroll_left(){
+    let right = document.querySelector('#International-movie')
     right.scrollBy(-350, 0)
+}
+
+function IMscroll_right(){
+    let left = document.querySelector('#International-movie')
+    left.scrollBy(350, 0)
+}
+
+function ISscroll_left(){
+    let right = document.querySelector('#International-tv-shows-anime')
+    right.scrollBy(-350, 0)
+}
+
+function ISscroll_right(){
+    let left = document.querySelector('#International-tv-shows-anime')
+    left.scrollBy(350, 0)
 }
